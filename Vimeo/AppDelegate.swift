7dc0b9f42839e2e7f6ad0e6495f2ea21eccc6f1f
Jisan Zaman <jisan@Jisans-MacBook-Pro.local>
2015-07-08 13:48:56 -0400

    first commit

--- Vimeo/AppDelegate.swift
+++ Vimeo/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = UIColor.yellowColor()
+        
+        self.window?.rootViewController = viewController
+        
+        self.window?.makeKeyAndVisible()
         return true
     }
 
