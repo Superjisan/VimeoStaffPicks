4b8ffdcc2cbe76f7d7ab1ef696c479bdd1671e69
Jisan Zaman <jisan@Jisans-MacBook-Pro.local>
2015-07-08 22:08:41 -0400

    fixed some UI issues, added navbar

--- Vimeo/StaffPicksViewController.swift
+++ Vimeo/StaffPicksViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class StaffPicksViewController: UIViewController {
-
+class StaffPicksViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView?
+    
+    let cellIdentifier = "CellIdentifier"
+    
+    let items = ["Jisan", "Geri", "Kate"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.yellowColor()
+        
+        self.title = "Staff Picks"
+        self.setupTableView()
+    }
+    
+    //MARK: Setup
+    func setupTableView(){
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    //MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
     }
 
 }
